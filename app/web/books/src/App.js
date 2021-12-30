536616858190cd9cc03149755502a544b42989b3
DevPio <luccaspio890@gmail.com>
2021-12-30 15:18:40 -0300

    create menu

--- app/web/books/src/App.js
+++ app/web/books/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
+import "./index.css";
+import Menu from "./Components/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <Switch>
+        <Route path="/" exact></Route>
+
+        <Route path="/createBook">
+          <div>Crie seu livro</div>
+        </Route>
+        <Route path="/searchBook">
+          <div>Busque seu livro</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
